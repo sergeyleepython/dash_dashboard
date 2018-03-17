7fdf94fe774b125e3e9694b0ec8af4a851d0dd17
Сергей Ли <s.lee@benovate.ru>
2018-03-18 02:39:32 +0300

    Make control elements work

--- dashboard.py
+++ dashboard.py
@@ -1,133 +1,142 @@
 import base64
 
 import dash
+from dash.dependencies import Input, Output, Event
 import dash_core_components as dcc
 import dash_html_components as html
-# from pandas_datareader.data import DataReader
-import time
-from collections import deque
 import plotly.graph_objs as go
-import random
 
-DATAFLOW = {
-    'Vibration FL': [0.5],
-    'Vibration FR': [0.5],
-    'Vibration RL': [0.5],
-    'Vibration RR': [0.5],
+COMPANY_NAME = 'MARTLET SOFTWARE'
+VEHICLES = ['SUV1', 'SUV2', 'Truck1']
+SUBASSIES_SENSORS = {
+    'BRAKING SYSTEM': ['Vibration FL', 'Vibration FR', 'Vibration RL', 'Vibration RR'],
+    'ELECTRIC SYSTEM': ['Battery Voltage']
 }
+SUBASSIES = list(SUBASSIES_SENSORS.keys())
 
-app = dash.Dash('vehicle-data')
+app = dash.Dash('vehicle-data', static_folder='assets')
+app.title = COMPANY_NAME
 
-max_length = 30
-times = deque(maxlen=max_length)
-vibration_FL = deque(maxlen=max_length)
-vibration_FR = deque(maxlen=max_length)
-vibration_RL = deque(maxlen=max_length)
-vibration_RR = deque(maxlen=max_length)
-
-data_dict = {
-    "Vibration FL": vibration_FL,
-    "Vibration FR": vibration_FR,
-    "Vibration RL": vibration_RL,
-    "Vibration RR": vibration_RR,
-}
-
-image_filename = 'images/logo.png'  # replace with your own image
+image_filename = 'assets/logo.png'  # replace with your own image
 encoded_image = base64.b64encode(open(image_filename, 'rb').read())
 
 app.layout = html.Div([
+    html.Div([html.H1(COMPANY_NAME),
+              html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()),
+                       style={'width': '200px'}),
+              ], className='row', style={'textAlign': 'center'}),
+
+    html.Div([html.H2(id='title'),
+              ], className='row', style={'textAlign': 'center'}),
+
     html.Div([
-        html.H2('Vehicle Data',
-                style={'float': 'left',
-                       }),
-    ]),
-    html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), style={'width': '200px', 'float': 'right'}),
-    dcc.Dropdown(id='vehicle-data-name',
-                 options=[{'label': s, 'value': s} for s in data_dict.keys()],
-                 value=['Vibration FL', 'Vibration FR', 'Vibration RL', 'Vibration RR'],
-                 multi=True
-                 ),
-    html.Div(children=html.Div(id='graphs'), className='row'),
+        html.Label('Vehicles'),
+        dcc.Dropdown(
+            id='vehicles-dropdown',
+            options=[{'label': s, 'value': s} for s in VEHICLES],
+            value=VEHICLES[0]
+        ),
+        html.Hr(),
+        html.Label('Subassies'),
+        dcc.RadioItems(
+            id='subassies-radioitems',
+            options=[{'label': s, 'value': s} for s in SUBASSIES],
+            value=SUBASSIES[0]
+        ),
+        html.Hr(),
+        html.Label('Sensors'),
+        dcc.Dropdown(id='sensors-dropdown',
+                     options=[{'label': s, 'value': s} for s in SUBASSIES_SENSORS[SUBASSIES[0]]],
+                     value=[],
+                     multi=True
+                     ),
+    ], className='row'),
+    html.Hr(),
+    html.Div(children=html.Div(id='graphs'),
+             className='row',
+             # style={'columnCount': 2}
+             ),
     dcc.Interval(id='graph-update', interval=5000),
-], className="container", style={'width': '98%', 'margin-left': 10, 'margin-right': 10, 'max-width': 50000})
-
-
-def update_obd_values(times, vibration_FL, vibration_FR, vibration_RL, vibration_RR):
-    times.append(time.time())
-    if len(times) == 1:
-        # starting relevant values
-        vibration_FL.append(0.5)
-        vibration_FR.append(0.5)
-        vibration_RL.append(0.5)
-        vibration_RR.append(0.5)
-    else:
-        for label, data_deque in data_dict.items():
-            l = DATAFLOW[label]
-            DATAFLOW[label] = [0.5]
-            data_deque.append(sum(l) / float(len(l)))
-
-    return times, vibration_FL, vibration_FR, vibration_RL, vibration_RR
-
-
-times, vibration_FL, vibration_FR, vibration_RL, vibration_RR = update_obd_values(times,
-                                                                                  vibration_FL,
-                                                                                  vibration_FR,
-                                                                                  vibration_RL,
-                                                                                  vibration_RR,
-                                                                                  )
-
-
-@app.callback(
-    dash.dependencies.Output('graphs', 'children'),
-    [dash.dependencies.Input('vehicle-data-name', 'value')],
-    events=[dash.dependencies.Event('graph-update', 'interval')]
+], className="container",
+    # style={'width': '98%', 'margin-left': 10, 'margin-right': 10, 'max-width': 50000}
 )
-def update_graph(data_names):
+
+
+@app.callback(Output('title', component_property='children'), [Input('vehicles-dropdown', 'value')])
+def update_vehicle(selected_dropdown_value):
+    return selected_dropdown_value
+
+
+@app.callback(Output('sensors-dropdown', 'options'), [Input('subassies-radioitems', 'value')])
+def update_subassy(selected_value):
+    options = [{'label': s, 'value': s} for s in SUBASSIES_SENSORS[selected_value]]
+    return options
+
+
+@app.callback(Output('sensors-dropdown', 'value'), [Input('subassies-radioitems', 'value')])
+def update_subassy(selected_value):
+    return SUBASSIES_SENSORS[selected_value]
+
+
+def get_sensor_data(sensor_name):
+    data = {"2018-01-01 01:01": 2.2,
+            "2018-01-01 01:02": 2.1,
+            "2018-01-01 01:03": 3.1
+            }
+    return data
+
+
+@app.callback(Output('graphs', 'children'), [Input('sensors-dropdown', 'value')],
+              events=[Event('graph-update', 'interval')])
+def update_graph(sensors):
     graphs = []
-    update_obd_values(times, vibration_FL, vibration_FR, vibration_RL, vibration_RR)
-
     class_choice = 'col s6 m6 l6'
-
-    for data_name in data_names:
+    for sensor in sensors:
+        sensor_data = get_sensor_data(sensor)
+        times = sensor_data.keys()
         data = go.Scatter(
             x=list(times),
-            y=list(data_dict[data_name]),
+            y=list(sensor_data.values()),
             name='Scatter',
             fill="tozeroy",
             fillcolor="#6897bb"
         )
 
         graphs.append(html.Div(dcc.Graph(
-            id=data_name,
+            id=sensor,
             animate=False,
-            figure={'data': [data], 'layout': go.Layout(xaxis=dict(range=[min(times), max(times)]),
-                                                        yaxis=dict(range=[0.0, 4.5]),
-                                                        margin={'l': 50, 'r': 1, 't': 45, 'b': 1},
-                                                        title='{}'.format(data_name),
-                                                        images=[dict(
-                                                            source='images/logo.png',
-                                                            xref="x",
-                                                            yref="y",
-                                                            x=0,
-                                                            y=3,
-                                                            sizex=2,
-                                                            sizey=2,
-                                                            sizing="fill",
-                                                            opacity=0.5,
-                                                            layer="above"
-                                                        )]), }
+            figure={'data': [data],
+                    'layout': go.Layout(xaxis=dict(range=[min(times), max(times)]),
+                                        yaxis=dict(range=[0.0, 4.5]),
+                                        margin={'l': 50, 'r': 1, 't': 45, 'b': 1},
+                                        title='{}'.format(sensor),
+                                        images=[dict(
+                                            source='/assets/logo.png',
+                                            xref="x",
+                                            yref="y",
+                                            x=0,
+                                            y=3,
+                                            sizex=2,
+                                            sizey=2,
+                                            sizing="fill",
+                                            opacity=0.5,
+                                            layer="above"
+                                        )]), }
         ), className=class_choice))
 
     return graphs
 
 
-external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css",
-                "my.css"]
+external_css = [
+    # "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css",
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
+    "/assets/my.css"
+]
 for css in external_css:
     app.css.append_css({"external_url": css})
 
 external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
-for js in external_css:
+for js in external_js:
     app.scripts.append_script({'external_url': js})
 
 if __name__ == '__main__':
